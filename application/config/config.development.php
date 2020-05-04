9c45fe0e44baf5ad740587b7964b91dd64af8839
Jack Wright <hello@builtontheweb.co.uk>
2020-05-04 21:05:36 +0100

    Sync File
    Dreamweaver Sync

--- application/config/config.development.php
+++ application/config/config.development.php
@@ -144,14 +144,14 @@ return array(
      * Configuration for: Email content data
      */
     'EMAIL_PASSWORD_RESET_URL' => 'login/verifypasswordreset',
-    'EMAIL_PASSWORD_RESET_FROM_EMAIL' => 'no-reply@example.com',
-    'EMAIL_PASSWORD_RESET_FROM_NAME' => 'My Project',
-    'EMAIL_PASSWORD_RESET_SUBJECT' => 'Password reset for PROJECT XY',
+    'EMAIL_PASSWORD_RESET_FROM_EMAIL' => 'no-reply@umsframework.com',
+    'EMAIL_PASSWORD_RESET_FROM_NAME' => 'User Management LITE',
+    'EMAIL_PASSWORD_RESET_SUBJECT' => 'Password reset for User Management LITE',
     'EMAIL_PASSWORD_RESET_CONTENT' => 'Please click on this link to reset your password: ',
     'EMAIL_VERIFICATION_URL' => 'register/verify',
-    'EMAIL_VERIFICATION_FROM_EMAIL' => 'no-reply@example.com',
-    'EMAIL_VERIFICATION_FROM_NAME' => 'My Project',
-    'EMAIL_VERIFICATION_SUBJECT' => 'Account activation for PROJECT XY',
+    'EMAIL_VERIFICATION_FROM_EMAIL' => 'no-reply@umsframework.com',
+    'EMAIL_VERIFICATION_FROM_NAME' => 'User Management LITE',
+    'EMAIL_VERIFICATION_SUBJECT' => 'Account activation for User Management LITE',
     'EMAIL_VERIFICATION_CONTENT' => 'Please click on this link to activate your account: ',
 	
 	/**
